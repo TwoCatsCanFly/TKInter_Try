e319dba0240fc18b8f1d879c48ab2c14083dee44
TwoCatsCanFly <TwoCatsCanFly@users.noreply.github.com>
2020-06-12 22:03:22 +0300

    Popup, new window

--- TKIT.py
+++ TKIT.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from PIL import ImageTk,Image
+from tkinter import messagebox
 root = Tk()
 root.title('TKInter Tips')
 
@@ -58,6 +59,58 @@ button_f = Button(frame, text = 'Im in a frame!!11').grid()
 root.iconbitmap('Windows.ico')
 myImg = ImageTk.PhotoImage(Image.open('Test_img.jpg'))
 my_Label = Label(frame, image=myImg).grid()
+
+# Радио кнопки))
+frame_for_radio = LabelFrame(root, text='Радио кнопки', padx=15, pady=15)
+frame_for_radio.grid(row=4, column=1, padx=10, pady=10)
+r = IntVar()
+r.set('2')
+
+def rClicked(value):
+    Label(frame_for_radio, text=value).pack()
+
+rButton = Radiobutton(frame_for_radio, text='Radiobutton 1', variable =r, value=1, command= lambda: rClicked(r.get())).pack()
+rButton1 = Radiobutton(frame_for_radio, text='Radiobutton 2', variable =r, value=2, command= lambda: rClicked(r.get())).pack()
+rButton2 = Radiobutton(frame_for_radio, text='Radiobutton 3', variable =r, value=3, command= lambda: rClicked(r.get())).pack()
+rButton3 = Radiobutton(frame_for_radio, text='Radiobutton 4', variable =r, value=4, command= lambda: rClicked(r.get())).pack()
+
+# Popup
+frame_for_Popup = LabelFrame(root, text='Popup', padx=15, pady=15)
+frame_for_Popup.grid(row=4, column=2, padx=10, pady=10)
+def popup1(): messagebox.showinfo('messagebox.showinfo ','Текст Информация')
+def popup2(): messagebox.showwarning('messagebox.showwarning ','Текст Предупреждение')
+def popup3(): messagebox.showerror('messagebox.showerror ','Текст Ошибка')
+def popup4():
+    responce = messagebox.askquestion('messagebox.askquestion ','Текст Вопрос')
+    Label(frame_for_Popup, text='ОТВЕТ ДА').pack() if responce == 'yes' else Label(frame_for_Popup, text='ОТВЕТ НЕТ').pack()
+def popup5():
+    responce = messagebox.askokcancel('messagebox.askokcancel ','Текст Отмена?')
+    Label(frame_for_Popup, text='ОТВЕТ OK').pack() if responce == 1 else Label(frame_for_Popup,
+                                                                                   text='ОТВЕТ CANCEL').pack()
+def popup6():
+    responce = messagebox.askyesno('messagebox.askyesno ','Текст Да или Нет')
+    Label(frame_for_Popup, text='ОТВЕТ ДА').pack() if responce == 1 else Label(frame_for_Popup,
+                                                                                   text='ОТВЕТ НЕТ').pack()
+
+pButton = Button(frame_for_Popup, text='showinfo', command=popup1).pack()
+pButton = Button(frame_for_Popup, text='showwarning', command=popup2).pack()
+pButton = Button(frame_for_Popup, text='showerror', command=popup3).pack()
+pButton = Button(frame_for_Popup, text='askquestion', command=popup4).pack()
+pButton = Button(frame_for_Popup, text='askokcancel', command=popup5).pack()
+pButton = Button(frame_for_Popup, text='askyesno', command=popup6).pack()
+
+# Дополнительное окно
+
+def nw_open():
+    global my_Img
+    nw = Toplevel()
+    nw.title('Second Window')
+    my_Img = ImageTk.PhotoImage(Image.open('Test_img.jpg'))
+    imgLabel = Label(nw, image=my_Img).grid()
+
+nw_btn = Button(root,text='New Window', command= nw_open).grid(row=4, column=3)
+
+
 
 # главный While True программы)))
 root.mainloop()
