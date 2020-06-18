81d5e8d6537134c359f5e0b8dd01f61b638c5880
TwoCatsCanFly <TwoCatsCanFly@users.noreply.github.com>
2020-06-18 10:54:55 +0300

    Clean up code

--- TKIT.py
+++ TKIT.py
@@ -7,43 +7,41 @@ root = Tk()
 root.title('TKInter Tips')
 #root.geometry('2000x1000')
 
-def loclick():
-    mylabel4 = Label(root, text='loclick Function Clicked')
-    mylabel4.grid(row=1, column=5)
-def pquit():
-    quit()
-def input_click():
-    input_label = Label(root, text='Entered text: ' + input_box.get())
-    input_label.grid(row=2, column=3)
-
 # Текст
-myLabel = Label(root, text='Hello world')
-myLabel2 = Label(root, text='TKInter')
-myLabel3 = Label(root, text='hello mr. Freeman')
+frame_for_label = LabelFrame(root, text='Лейблы, текст', padx=15, pady=15)
+frame_for_label.grid(row=0, column=0, padx=10, pady=10)
+myLabel = Label(frame_for_label, text='Hello world').pack()
+myLabel2 = Label(frame_for_label, text='TKInter').pack()
+myLabel3 = Label(frame_for_label, text='hello mr. Freeman').pack()
 
 # Кнопки
-button = Button(root, text='Doubt')
-button1 = Button(root, text='disabled',state='disabled')
-button2 = Button(root, text='padx=50',padx=50)
-button3 = Button(root, text='padx=50, pady=50',padx=50, pady=50).grid(row=1, column=3)
-button4 = Button(root, text='loclick Function', command=loclick).grid(row=1, column=4)
-button5 = Button(root, text='Quit Function', command=pquit).grid(row=1, column=6)
-button6 = Button(root, text='fg=\'red\',bg=\'black\'', fg='red',bg='black').grid(row=2, column=0)
-button7 = Button(root, text='root.quit',command = root.quit).grid(row=3, column=0)
+def pquit():
+    quit()
+def loclick():
+    mylabel4 = Label(frame_for_button, text='loclick Function Clicked')
+    mylabel4.pack()
+frame_for_button = LabelFrame(root, text='Кнопки', padx=15, pady=15)
+frame_for_button.grid(row=0, column=1, padx=10, pady=10)
+button = Button(frame_for_button, text='Doubt').pack()
+button1 = Button(frame_for_button, text='disabled',state='disabled').pack()
+button2 = Button(frame_for_button, text='padx=50',padx=50).pack()
+button3 = Button(frame_for_button, text='padx=50, pady=50',padx=50, pady=50).pack()
+button4 = Button(frame_for_button, text='loclick Function', command=loclick).pack()
+button5 = Button(frame_for_button, text='Quit Function', command=pquit).pack()
+button6 = Button(frame_for_button, text='fg=\'red\',bg=\'black\'', fg='red',bg='black').pack()
+button7 = Button(frame_for_button, text='root.quit',command = root.quit).pack()
 
 # Ввод
-input_box = Entry(root,width=50,borderwidth='10')
+def input_click():
+    input_label = Label(frame_for_input, text='Entered text: ' + input_box.get())
+    input_label.pack()
+frame_for_input = LabelFrame(root, text='Ввод', padx=15, pady=15)
+frame_for_input.grid(row=1, column=0, padx=10, pady=10)
+input_box = Entry(frame_for_input,width=50,borderwidth='10')
 input_box.insert(0,'input_box')
-input_box.grid(row=2, column=1)
-input_button = Button(root, text='Enter Text', fg='black',bg='orange',command=input_click).grid(row=2, column=2)
+input_box.pack()
+input_button = Button(frame_for_input, text='Enter Text', fg='black',bg='orange',command=input_click).pack()
 
-# Отображение созданных обьектов, можно по другому
-myLabel.grid(row=0, column=0)
-myLabel2.grid(row=0, column=1)
-myLabel3.grid(row=0, column=2)
-button.grid(row=1, column=0)
-button1.grid(row=1, column=1)
-button2.grid(row=1, column=2)
 
 # Статус
 status = Label(root,
@@ -55,7 +53,7 @@ status.grid(row=10, column=0, columnspan = 7, sticky = W+E)
 
 # Рамка
 frame = LabelFrame(root, text='Frame,  padx=5, pady=5', padx=15, pady=15)
-frame.grid(row=4, column=0, padx=10, pady=10)
+frame.grid(row=0, column=2, padx=10, pady=10)
 button_f = Button(frame, text = 'Im in a frame!!11').grid()
 
 # картинки. поддержка gif встроенная
@@ -65,7 +63,7 @@ my_Label = Label(frame, image=myImg).grid()
 
 # Радио кнопки))
 frame_for_radio = LabelFrame(root, text='Радио кнопки', padx=15, pady=15)
-frame_for_radio.grid(row=4, column=1, padx=10, pady=10)
+frame_for_radio.grid(row=1, column=1, padx=10, pady=10)
 r = IntVar()
 r.set('2')
 
@@ -79,7 +77,7 @@ rButton3 = Radiobutton(frame_for_radio, text='Radiobutton 4', variable =r, value
 
 # Popup
 frame_for_Popup = LabelFrame(root, text='Popup', padx=15, pady=15)
-frame_for_Popup.grid(row=4, column=2, padx=10, pady=10)
+frame_for_Popup.grid(row=1, column=2, padx=10, pady=10)
 def popup1(): messagebox.showinfo('messagebox.showinfo ','Текст Информация')
 def popup2(): messagebox.showwarning('messagebox.showwarning ','Текст Предупреждение')
 def popup3(): messagebox.showerror('messagebox.showerror ','Текст Ошибка')
@@ -95,11 +93,11 @@ def popup6():
     Label(frame_for_Popup, text='ОТВЕТ ДА').pack() if responce == 1 else Label(frame_for_Popup,
                                                                                    text='ОТВЕТ НЕТ').pack()
 pButton = Button(frame_for_Popup, text='showinfo', command=popup1).pack()
-pButton = Button(frame_for_Popup, text='showwarning', command=popup2).pack()
-pButton = Button(frame_for_Popup, text='showerror', command=popup3).pack()
-pButton = Button(frame_for_Popup, text='askquestion', command=popup4).pack()
-pButton = Button(frame_for_Popup, text='askokcancel', command=popup5).pack()
-pButton = Button(frame_for_Popup, text='askyesno', command=popup6).pack()
+pButton1 = Button(frame_for_Popup, text='showwarning', command=popup2).pack()
+pButton2 = Button(frame_for_Popup, text='showerror', command=popup3).pack()
+pButton3 = Button(frame_for_Popup, text='askquestion', command=popup4).pack()
+pButton4 = Button(frame_for_Popup, text='askokcancel', command=popup5).pack()
+pButton5 = Button(frame_for_Popup, text='askyesno', command=popup6).pack()
 
 # Дополнительное окно
 def nw_open():
@@ -109,7 +107,9 @@ def nw_open():
     my_Img = ImageTk.PhotoImage(Image.open('Test_img.jpg'))
     Label(nw, image=my_Img).grid()
     Button(nw,text='DESTROY WINDOW!!!11', command=nw.destroy).grid()
-nw_btn = Button(root,text='New Window', command= nw_open).grid(row=4, column=3)
+frame_for_window = LabelFrame(root, text='Новое Окно', padx=15, pady=15)
+frame_for_window.grid(row=1, column=3, padx=10, pady=10)
+nw_btn = Button(frame_for_window,text='New Window', command= nw_open).pack()
 
 # Открытие файлов
 def f_open():
@@ -124,7 +124,7 @@ def f_open():
         Label(frame_for_file, image=f_image).pack()
 
 frame_for_file = LabelFrame(root, text='File', padx=15, pady=15)
-frame_for_file.grid(row=4, column=4, padx=10, pady=10)
+frame_for_file.grid(row=1, column=4, padx=10, pady=10)
 f_button = Button(frame_for_file, text='Open File',command=f_open).pack()
 
 # Слайдеры
@@ -132,7 +132,7 @@ def slide(x=None):
     Label(frame_for_slider,text='vertical: '+ str(vertical.get())).grid(row=2, column=0)
     Label(frame_for_slider,text='horizontal: '+ str(horizontal.get())).grid(row=2, column=1)
 frame_for_slider = LabelFrame(root, text='Slider', padx=15, pady=15)
-frame_for_slider.grid(row=4, column=5, padx=10, pady=10)
+frame_for_slider.grid(row=0, column=3, padx=10, pady=10)
 vertical = Scale(frame_for_slider, from_=0, to=100,command=slide)
 horizontal = Scale(frame_for_slider, from_=0, to=100, orient=HORIZONTAL,command=slide)
 slide()
@@ -141,7 +141,7 @@ horizontal.grid(row=1, column=0)
 
 # Чекбоксы
 frame_for_checkbox = LabelFrame(root, text='Чекбоксы', padx=15, pady=15)
-frame_for_checkbox.grid(row=4, column=6, padx=10, pady=10)
+frame_for_checkbox.grid(row=1, column=5, padx=10, pady=10)
 def chck(val):
     Label(frame_for_checkbox, text=val.get()).pack()
 chck_var = StringVar()
@@ -151,7 +151,7 @@ chck_btn.pack()
 
 # Выпадающий список
 frame_for_droplist = LabelFrame(root, text='Дроплист', padx=15, pady=15)
-frame_for_droplist.grid(row=4, column=7, padx=10, pady=10)
+frame_for_droplist.grid(row=0, column=5, padx=10, pady=10)
 def drop_lst(x):
     Label(frame_for_droplist, text=choosenone.get()).pack()
 options = ['one','two','three','OMG, 2020 WKUA']
@@ -162,7 +162,7 @@ drop = OptionMenu(frame_for_droplist, choosenone, *options,command=drop_lst).pac
 # База данных
 dbase = 'address_book.db'
 frame_for_database = LabelFrame(root, text='Дроплист', padx=15, pady=15)
-frame_for_database.grid(row=4, column=8, padx=10, pady=10)
+frame_for_database.grid(row=0, column=4, padx=10, pady=10)
 conn = sqlite3.connect(dbase) # create database + connect to it
 c = conn.cursor() # create cursor
 
